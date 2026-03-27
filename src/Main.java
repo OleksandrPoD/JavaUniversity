4433a88c617de338aec01fbf60dc0a2114f4a620
OleksandrPoD <sashapodusovskyi2006@gmail.com>
2026-03-27 15:28:19 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double r = Double.parseDouble(args[0]);
+        double L = 2 * Math.PI * r;
+
+        System.out.println("Довжина кола: " + L);
+    }
+}
